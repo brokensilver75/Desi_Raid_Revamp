b1b9e5fb1a86dbed2e6b7db49468f42490b9066a
R S Madhan <rsmofficial75@gmail.com>
2026-04-12 05:47:11 +0530

    added walking animation for hub gameplay

--- Desi Raid Revamp/Assets/Scripts/Player_Animation_Controller.cs
+++ Desi Raid Revamp/Assets/Scripts/Player_Animation_Controller.cs
@@ -11,6 +11,10 @@ public class Player_Animation_Controller : MonoBehaviour
     private Vector2 move_direction;
     private Vector3 local_move_vector;
 
+    delegate void Animate_Player_Delegate();
+    Animate_Player_Delegate animate_player_delegate;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,22 +23,37 @@ public class Player_Animation_Controller : MonoBehaviour
 
     private void Update()
     {
+        animate_player_delegate?.Invoke();
+        //Animate_Player_Combat();
+    }
+
+    private void Animate_Player_Combat()
+    {
         local_move_vector = transform.InverseTransformDirection(new Vector3(move_direction.x, 0, move_direction.y));
         player_animator.SetFloat("UpDown", Mathf.Lerp(player_animator.GetFloat("UpDown"), local_move_vector.z, anim_smooth_multiplier));
         player_animator.SetFloat("LeftRight", Mathf.Lerp(player_animator.GetFloat("LeftRight"), local_move_vector.x, anim_smooth_multiplier));
     }
 
+    private void Animate_Player_Hub()
+    {
+        float magnitude = move_direction.magnitude;
+        magnitude = Mathf.Ceil(magnitude);
+        player_animator.SetFloat("Moving", magnitude);
+    }
+
     private void Handle_Game_State_Change(GameStates states)
     {
-       /* switch (states)
+        switch (states)
         {
             case GameStates.LEVEL_PLAY:
-                player_animator.SetBool("isHub", false);
+                player_animator.SetBool("hub_play", false);
+                animate_player_delegate = Animate_Player_Combat;
                 break;
             case GameStates.HUB_PLAY:
-                player_animator.SetBool("isHub", true);
+                player_animator.SetBool("hub_play", true);
+                animate_player_delegate = Animate_Player_Hub;
                 break;
-        }*/
+        }
     }
 
     public void OnMove(InputAction.CallbackContext callbackContext)
