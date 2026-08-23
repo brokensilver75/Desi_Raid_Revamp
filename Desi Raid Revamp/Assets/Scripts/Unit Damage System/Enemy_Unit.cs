615546c89ee7d5e56c4da67e9edbe24d5beb229a
R S Madhan <118668914+brokensilver75@users.noreply.github.com>
2026-08-23 13:17:17 +0530

    Enemy dummy

--- Desi Raid Revamp/Assets/Scripts/Unit Damage System/Enemy_Unit.cs
+++ Desi Raid Revamp/Assets/Scripts/Unit Damage System/Enemy_Unit.cs
@@ -1,11 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class Enemy_Unit : Unit
 {
+    [SerializeField] private TextMeshProUGUI health_text;
+
+    private void Start()
+    {
+        health_text.SetText(max_health.ToString());
+    }
+
     public override void Take_Damage(float damage)
     {
         base.Take_Damage(damage);
         Debug.Log($"[Enemy_Unit] Enemy unit took {damage} damage. Current health: {current_health}");
+        health_text.SetText(current_health.ToString());
     }
 
     public override void Die()
